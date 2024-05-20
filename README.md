eac0e7d09ab67d84abf65b792e8dba051a1ad346
Tushar Gahlaut <tushargahlaut74@gmail.com>
2024-05-20 21:53:01 +0530

    Start with Delete

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ A Stock Market Forum
 Todo:
 - Basic Auth for User [Login, Register] is done
 - Google Auth is Done
-- Functionality for Creating Posts, Comments.
+- Functionality for Creating Posts, Comments is done.
+- Currently working on Edit and Delete Posts.
+- Making it mobile responsive side by side
 - NIFTY API Done, Able to achieve upto 52% Faster Response speeds when using redis in between,
 - NIFTY Integration on Frontend Done
 - Cloudinary Integration Done
-- Started with Building Frontend, Posts Page Create Post Component done.
+- Started with Building Frontend, Posts Page Create Post Component done <- Only Edit Delete Remaining
--- client/src/components/posts/single-post/edit-delete.tsx
+++ client/src/components/posts/single-post/edit-delete.tsx
@@ -1,14 +1,53 @@
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import { GripVertical, Pencil, Trash2 } from "lucide-react";
+import { useState } from "react";
+
+function DeletePost() {
+    const [deleteOpen, setDeleteOpen] = useState<boolean>(false);
+    async function deletePost(){
+        //Functionality to Delete Post
+    }
+  return (
+    <Dialog open={deleteOpen} onOpenChange={setDeleteOpen}>
+      <DialogTrigger asChild>
+        <div className="flex items-center m-1 cursor-pointer hover:bg-secondary rounded p-1">
+          <Trash2 />
+          <span className="sm:hidden md:inline ml-2 ">Delete</span>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete this post?</DialogTitle>
+          <DialogDescription>This action cannot be undone!</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={()=>setDeleteOpen(false)} variant={"secondary"}>Cancel</Button>
+          <Button onClick={deletePost} variant="default">Delete</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
 
 export function EditDeletePost() {
   return (
@@ -18,15 +57,12 @@ export function EditDeletePost() {
           <NavigationMenuTrigger>
             <GripVertical />
           </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <div className="flex items-center m-1 cursor-pointer hover:bg-secondary rounded p-2">
+          <NavigationMenuContent className="p-2">
+            <div className="flex items-center m-1 cursor-pointer hover:bg-secondary rounded p-1">
               <Pencil />
               <span className="ml-2 sm:hidden md:inline">Edit</span>
             </div>
-            <div className="flex items-center m-1 cursor-pointer hover:bg-secondary rounded p-2">
-              <Trash2 />
-              <span className="ml-2 sm:hidden md:inline">Delete</span>
-            </div>
+            <DeletePost />
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
